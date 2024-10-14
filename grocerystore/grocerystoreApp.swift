9cdcf7767322c41cef7599c1a8af48e2e6004218
Muhammad Mukthayar <khanadil1000s@gmail.com>
2024-10-14 12:12:40 +0500

    Initial Commit

---
+++ grocerystore/grocerystoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  grocerystoreApp.swift
+//  grocerystore
+//
+//  Created by Muhammad Mukhtayar on 14/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct grocerystoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
